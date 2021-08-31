b6f9a6aac07c71c9bbd36155b1125ad097304a63
BelenMaru <bleneee@gmail.com>
2021-08-31 05:26:55 +0000

    Final  redirect

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -21,26 +22,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {   // Get rout to show form
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  // console.log(req.params.shortURL);
-  // console.log(urlDatabase[req.params.shortURL]);
-
+app.get("/urls/:shortURL", (req, res) => {     // Add the second route and template
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -52,9 +45,16 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;  // POST request ot urlDatabase
+  res.redirect('urls/' + shortURL);
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
 
 
 app.listen(PORT, () => {
